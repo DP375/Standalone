62b6bfcfb01b636fb3b30c902faf361babe3de40
DP375 <dobrivojp375@gmail.com>
2023-09-18 14:18:33 +0200

    Partially corrected code

--- Todo-script.js
+++ Todo-script.js
@@ -1,30 +1,33 @@
 // Dodavanje "close" dugmeta za svaku stavku liste
 const SPAN_CLOSE = "<span class='close'>\u00D7</span>";
-var promenjiva = $("#myInput");
+var variable = $("#userInput");
+
+// Ciljanje <ul> preko klase "todo-list"
+var todoList = $(".todo-list");
 
 $("li").each(function () {
   $(this).append(SPAN_CLOSE);
 });
 
 // Klik na "close" dugme skriva trenutnu stavku
-$("ul").on("click", ".close", function () {
+todoList.on("click", ".close", function () {
   $(this).parent().css("display", "none");
 });
 
 // Klik na stavku označava je kao "checked"
-$("ul").on("click", "li", function () {
+todoList.on("click", "li", function () {
   $(this).toggleClass("checked");
 });
 
 // Dodavanje nove stavke na listu
 $(".add-btn").click(function () {
-  var inputValue = promenjiva.val();
-  if (inputValue === "") {
+  var inputValue = variable.val();
+  if (!inputValue) {
     showError("You must enter something on To-do list.");
   } else {
     var listItem = $("<li>").text(inputValue).append(SPAN_CLOSE);
-    $("#myUL").append(listItem);
-    promenjiva.val("");
+    todoList.append(listItem);
+    variable.val("");
   }
 });
 
@@ -36,9 +39,9 @@ function showError(message) {
     errorPopup.fadeOut(function () {
       $(this).remove();
     });
-  }, 1500); // Poruka će nestati nakon 1 sekunde (1000ms)
+  }, 1500);
 }
+
 $("#clearList").click(function () {
-  // Koristi jQuery za brisanje svih <li> elemenata unutar <ul> elementa
-  $("#myUL").empty();
+  todoList.empty();
 });
