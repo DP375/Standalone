f8ae942998c7eee3f8b97f1a247405f6444ec360
DP375 <dobrivojp375@gmail.com>
2023-09-21 11:35:59 +0200

    Should I get rid of 'this' in the click event functions for 'checked' and 'close' with 'event.currentTarget'?

--- Todo-script.js
+++ Todo-script.js
@@ -1,37 +1,43 @@
-window.onload = function () {
+$(document).ready(() => {
   // Dodajte svoj kod koji želite da se izvrši nakon učitavanja stranice ovdje
   const SPAN_CLOSE = "<span class='close'>\u00D7</span>";
   const POPOUP_TIME = 1500;
   const POPUP_ERROR = "<div class='error-popup'>";
   const ERROR_MESSAGE = "You must enter something on To-do list";
-
-  var user_input = $("#userInput");
+  const USER_INPUT = $("#userInput");
 
   // Ciljanje <ul> preko klase "todo-list"
-  var todoList = $(".todo-list");
-  $("li").each(function () {
-    $(this).append(SPAN_CLOSE);
-  });
+  const todoList = $(".todo-list");
+  $(".todoList li").each((element) => $(element).append(SPAN_CLOSE));
+
+  //ili
+  //todoList.find("li").each((element) => $(element).append(SPAN_CLOSE));
 
   // Klik na "close" dugme skriva trenutnu stavku
   todoList.on("click", ".close", function () {
     $(this).parent().css("display", "none");
   });
+  // todoList.on("click", ".close", () => {
+  //   $(event.currentTarget).parent().toggle();
+  // });
 
   // Klik na stavku označava je kao "checked"
   todoList.on("click", "li", function () {
     $(this).toggleClass("checked");
   });
+  // todoList.on("click", "li", () => {
+  //   $(event.currentTarget).toggleClass("checked");
+  // });
 
   // Dodavanje nove stavke na listu
-  $(".add-btn").click(function () {
-    var inputValue = user_input.val();
-    if (!inputValue) {
+  $(".add-btn").click(() => {
+    const INPUT_VALUE = USER_INPUT.val();
+    if (!INPUT_VALUE) {
       showError(ERROR_MESSAGE);
     } else {
-      var listItem = $("<li>").text(inputValue).append(SPAN_CLOSE);
-       $(".todo-list").append(listItem);
-      user_input.val("");
+      var listItem = $("<li>").text(INPUT_VALUE).append(SPAN_CLOSE);
+      $(".todo-list").append(listItem);
+      USER_INPUT.val("");
     }
   });
 
@@ -39,14 +45,14 @@ window.onload = function () {
     const ERROR_POPUP = $(POPUP_ERROR).text(message);
     $("body").append(ERROR_POPUP);
 
-    setTimeout(function () {
-      ERROR_POPUP.fadeOut(function () {
+    setTimeout(() => {
+      ERROR_POPUP.fadeOut(() => {
         $(this).remove();
       });
     }, POPOUP_TIME);
   }
 
-  $("#clearList").click(function () {
+  $("#clearList").click(() => {
     todoList.empty();
   });
-};
+});
