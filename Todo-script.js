c0f6c7d01be13527887b3b62f92037967075608e
DP375 <dobrivojp375@gmail.com>
2023-09-21 12:34:05 +0200

    samo obrisao nepotrebno

--- Todo-script.js
+++ Todo-script.js
@@ -1,27 +1,21 @@
 $(document).ready(() => {
-  // Dodajte svoj kod koji želite da se izvrši nakon učitavanja stranice ovdje
   const SPAN_CLOSE = "<span class='close'>\u00D7</span>";
   const POPOUP_TIME = 1500;
   const POPUP_ERROR = "<div class='error-popup'>";
   const ERROR_MESSAGE = "You must enter something on To-do list";
   const USER_INPUT = $("#userInput");
 
-  // Ciljanje <ul> preko klase "todo-list"
   const todoList = $(".todo-list");
-  // $(".todo-list li").each((element) => $(element).append(SPAN_CLOSE));
   todoList.find("li").each((element) => $(element).append(SPAN_CLOSE));
 
-  // Klik na "close" dugme skriva trenutnu stavku
   todoList.on("click", ".close", (event) => {
     $(event.currentTarget).parent().toggle();
   });
 
-  // Klik na stavku označava je kao "checked"
   todoList.on("click", "li", (event) => {
     $(event.currentTarget).toggleClass("checked");
   });
 
-  // Dodavanje nove stavke na listu
   $(".add-btn").click(() => {
     const INPUT_VALUE = USER_INPUT.val();
     if (!INPUT_VALUE) {
